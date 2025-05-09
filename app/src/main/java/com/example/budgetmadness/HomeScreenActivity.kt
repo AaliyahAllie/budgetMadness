cc501b4d5c1ec40c997388b434f38c6cbe304933
Aaliyah <ST10212542@RCCONNECT.EDU.ZA>
2025-05-09 11:06:00 +0200

    home screen created where information of expenses, income, balance will be displayed

---
+++ app/src/main/java/com/example/budgetmadness/HomeScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.example.budgetmadness
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_income -> {
+                    startActivity(Intent(this, IncomeActivity::class.java))
+                    true
+                }
+                R.id.nav_home -> {
+                    startActivity(Intent(this, StarterPageActivity::class.java))
+                    true
+                }
+                R.id.nav_add -> {
+                    startActivity(Intent(this, AddExpensesActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
