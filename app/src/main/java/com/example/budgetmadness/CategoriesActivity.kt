99876717c84cfbc1bfa728867b57cc2511e9e78f
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2025-05-10 13:51:08 +0200

    CategoriesActivity logic added for everything and database connected

--- app/src/main/java/com/example/budgetmadness/CategoriesActivity.kt
+++ app/src/main/java/com/example/budgetmadness/CategoriesActivity.kt
@@ -4,20 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.*
 
 class CategoriesActivity : AppCompatActivity() {
+
+    private lateinit var dbHelper: BudgetDatabaseHelper
+    private lateinit var categoryListView: ListView
+    private lateinit var newCategoryInput: EditText
+    private lateinit var addCategoryButton: Button
+    private lateinit var adapter: ArrayAdapter<String>
+    private lateinit var categories: MutableList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_categories)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        dbHelper = BudgetDatabaseHelper(this)
+
+
+
+
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
