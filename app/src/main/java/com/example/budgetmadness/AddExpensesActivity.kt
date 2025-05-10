c7e1cf65474c9e24abdc589f5cf09940d88d8906
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2025-05-10 10:26:35 +0200

    private lateinit var edittext, spinner and button added to AddExpenseActivity.kt

--- app/src/main/java/com/example/budgetmadness/AddExpensesActivity.kt
+++ app/src/main/java/com/example/budgetmadness/AddExpensesActivity.kt
@@ -5,8 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.*
+import java.util.*
 
 class AddExpensesActivity : AppCompatActivity() {
+
+    private lateinit var expenseNameInput: EditText
+    private lateinit var expenseAmountInput: EditText
+    private lateinit var paymentMethodInput: EditText
+    private lateinit var categorySpinner: Spinner
+    private lateinit var selectDataButton: Button
+    private lateinit var uploadReceiptButton: Button
+    private lateinit var addExpenseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
