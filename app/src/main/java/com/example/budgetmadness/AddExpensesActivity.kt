5a8fa0d1de8e20aca7531cdee38758be7169a8f0
Amaan Williams <129081794+ST10396397@users.noreply.github.com>
2025-05-10 13:09:21 +0200

    Link views added to AddExpensesActivity.kt

--- app/src/main/java/com/example/budgetmadness/AddExpensesActivity.kt
+++ app/src/main/java/com/example/budgetmadness/AddExpensesActivity.kt
@@ -22,6 +22,32 @@ class AddExpensesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_expenses)
+
+        val dbHelper = ExpenseDatabaseHelper(this)
+        val categories = dbHelper.getAllCategories()
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        categorySpinner.adapter = adapter
+
+        //LINK VIEWS
+        expenseNameInput = findViewById(R.id.expenseNameInput)
+        expenseAmountInput = findViewById(R.id.expenseAmountInput)
+        paymentMethodInput = findViewById(R.id.paymentMethodInput)
+        categorySpinner = findViewById(R.id.categorySpinner)
+        selectDataButton = findViewById(R.id.selectDateButton)
+        uploadReceiptButton = findViewById(R.id.uploadReceiptButton)
+        addExpenseButton = findViewById(R.id.addExpenseButton)
+
+        //SPINNER SETUP
+
+
+        //DATE PICKER
+
+
+        //ADD EXPENSE
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
